a1a591f6a1e830bc57f76c035dc8e4d487d1c52c
Simon Libut <anthonylibut@gmail.com>
2024-10-02 18:06:51 +0800

    Initial commit

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -6,13 +6,13 @@ import reader.MapData;
 
 public class Driver {
   public static void main(String[] args) {
-    /*if (args.length < 2) {
+    if (args.length < 2) {
       System.err.println("Usage: Driver <map name> <mode>");
       System.exit(1);
-    }*/
+    }
 
-    String mapName = "testlevel";//args[0];
-    String mode = "bot"; //args[1];
+    String mapName = args[0];
+    String mode = args[1];
 
     FileReader fileReader = new FileReader();
     MapData mapData = fileReader.readFile(mapName);
--- src/solver/SokoBot.java
+++ src/solver/SokoBot.java
@@ -4,7 +4,7 @@ public class SokoBot {
 
 
   public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
-    return null;
+    return "lrlrlrlrlrlrlrlrlrlrlrlrlrlrlr";
   }
 
 }
