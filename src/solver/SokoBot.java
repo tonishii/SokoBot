1d33472b8cf64d03c95a3595ab54f49321caba73
Simon Libut <anthonylibut@gmail.com>
2024-10-03 14:12:23 +0800

    Added way to create state space

--- src/solver/SokoBot.java
+++ src/solver/SokoBot.java
@@ -1,10 +1,29 @@
 package solver;
 
+import reader.MapData;
+
 public class SokoBot {
+  private StateNode treeRoot;
 
-
-  public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
-    return "lrlrlrlrlrlrlrlrlrlrlrlrlrlrlr";
+  private int eval() {
+    return 0;
   }
 
+  private void createGameSpace(int width, int height, char[][] mapData, char[][] itemsData) {
+    MapData initState = new MapData();
+    initState.columns = width;
+    initState.rows = height;
+    initState.tiles = mapData;
+
+    // TEMPORARY
+    this.treeRoot = new StateNode(initState, eval());
+  }
+
+  public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
+    // 1. Map out all the states
+    // 2. Prune subtrees or nodes that are redundant
+    // 3. Search for the solution
+
+    return "lrlrlrlrlrlrlrlrlrlrlrlrlrlrlr";
+  }
 }
