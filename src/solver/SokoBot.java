0d82ac3d7487f97f319825eba1cb91bd92cddafb
Simon Libut <anthonylibut@gmail.com>
2024-10-16 22:55:33 +0800

    Added starting point for DFS search and other functions

--- src/solver/SokoBot.java
+++ src/solver/SokoBot.java
@@ -1,31 +1,17 @@
 package solver;
 
 import java.util.ArrayList;
-import reader.MapData;
+import java.util.Stack;
 
 class Coordinate {
-  public int x,
-             y;
+  public int x, y;
 
   public Coordinate(int x, int y) {
     this.x = x;
     this.y = y;
   }
-
-  public Coordinate(Coordinate pos, char dir) {
-    this.x = x;
-    this.y = y;
-
-    if (dir == 'l')
-      this.x--;
-    else if (dir == 'r')
-      this.x++;
-    else if (dir == 'f')
-      this.y--;
-    else if (dir == 'b')
-      this.y++;
-  }
 }
+
 class Board {
   public char[][] mapData;
   public char[][] itemData;
@@ -45,10 +31,30 @@ class Push {
     this.box_index = box_index;
     this.dir = dir;
   }
+
+  public Coordinate pushBox(Coordinate box) {
+    int x = box.x;
+    int y = box.y;
+
+    if (dir == 'u')
+      y--;
+
+    else if (dir == 'r')
+      x++;
+
+    else if (dir == 'd')
+      y++;
+
+    else if (dir == 'l')
+      y--;
+
+    return new Coordinate(x, y);
+  }
 }
 
 public class SokoBot {
-  private Board initS;
+  private State initState;
+  private Board initBoard;
   private Coordinate start_player_pos;
 
   private ArrayList<Coordinate> targets;
@@ -67,79 +73,96 @@ public class SokoBot {
    * isEnd returns true if the game has already reached goal state
    * CURRENTLY ONLY CHECKS IF ALL THE CRATES ARE IN THE CORRECT POSITION BUT
    * DOES NOT CONSIDER CHARACTERS POSITION
+   *
    * @param width
    * @param height
    * @param itemsData
    * @return
    */
   private boolean isEnd(State s) {
-    for (Coordinate boxes : s.box_pos_list) {
-        if (this.board.mapData[boxes.y][boxes.x] != BoardValues.TARGET.value)
+    for (Coordinate boxes : s.crate_pos_list) {
+      if (s.board.mapData[boxes.y][boxes.x] != BoardValues.TARGET.value)
         return false;
     }
     return true;
   }
 
-  private void move(State s, Push push) {
+  private State move(State s, Push push) {
     // get the current index of the starting/origin pos of the box
-    Coordinate start_box = s.box_pos_list.get(push.box_index);
+    Coordinate start_box = s.crate_pos_list.get(push.box_index);
+    Coordinate dest_box = push.pushBox(start_box);
 
-    Coordinate dest_box = new Coordinate(start_box, push.dir);
+    // first set the new position of the box
+    s.crate_pos_list.set(push.box_index, dest_box);
+
+    // then reflect it onto the states board
+    s.board.itemData[start_box.y][start_box.x] = BoardValues.EMPTY.value;
+    s.board.itemData[dest_box.y][dest_box.x] = BoardValues.CRATE.value;
+
+    s.prevPush = push;
+
+    return s;
   }
 
-  // private int[][] h(State s, int height, int width) {
-  //   Board board = s.board;
-  //   for (int i = 0; i < height; i++) {
-  //     for (int j = 0; j < width; i++) {
+  private boolean isSquareFree(Board board, int y, int x) {
+    return board.mapData[y][x] != BoardValues.WALL.value &&
+        board.itemData[y][x] != BoardValues.CRATE.value &&
+        board.itemData[y][x] != BoardValues.PLAYER.value;
+  }
 
-  //     }
-  //   }
-  // }
   private boolean[] findLegalPushes(Coordinate box, Board board) {
-      boolean[] adjacent = new boolean[4];
-      // up
-      if (board.mapData[box.y - 1][box.x] != BoardValues.WALL.value &&
-          board.mapData[box.y - 1][box.x] != BoardValues.CRATE.value) {
-          adjacent[0] = true;
-      }
-      // down
-      if (board.mapData[box.y][box.x + 1] != BoardValues.WALL.value &&
-          board.mapData[box.y][box.x + 1] != BoardValues.CRATE.value) {
-        adjacent[1] = true;
-      }
-      // left
-      if (board.mapData[box.y - 1][box.x - 1] != BoardValues.WALL.value &&
-          board.mapData[box.y - 1][box.x - 1] != BoardValues.CRATE.value) {
-        adjacent[2] = true;
-      }
-      // right
-      if (board.mapData[box.y][box.x + 1] != BoardValues.WALL.value &&
-          board.mapData[box.y][box.x + 1] != BoardValues.CRATE.value) {
-        adjacent[3] = true;
-      }
+    boolean[] adjacent = new boolean[4];
+    int[][] directions = {
+        {-1, 0}, // up
+        {0, 1}, // right
+        {1, 0}, // down
+        {0, -1} // left
+    };
 
-      return adjacent;
+    for (int i = 0; i < 4; i++) {
+      if (isSquareFree(board, box.y + directions[i][0],
+          box.x + directions[i][1])) {
+        adjacent[i] = true;
+      }
+    }
+
+    return adjacent;
   }
 
   private ArrayList<State> createLegalPush(State s) {
     ArrayList<State> legalPushes = new ArrayList<>();
 
-    for (Coordinate box : s.box_pos_list) {
-      for (boolean legalDir : findLegalPushes(box, s.board)) {
-        if (legalDir == true)
-          legalPushes.add(new State())
+    // Go through every box's positions
+    for (Coordinate box : s.crate_pos_list) {
+      // Check every legal direction the box can go in
+      boolean[] legalDir = findLegalPushes(box, s.board);
+
+      for (int i = 0; i < 4; i++) {
+        // Iterate over the directions {0 = up, 1 = right, 2 = down, 3 = left}
+        if (legalDir[i] == true) {
+          char dir = (i == 0) ? 'u' : (i == 1) ? 'r' : (i == 2) ? 'd' : 'l';
+          legalPushes.add(move(s, new Push(s.crate_pos_list.indexOf(box), dir)));
+        }
       }
     }
+    return legalPushes;
   }
 
   public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
-    // 1. Map out all the states
-    // 2. Prune subtrees or nodes that are redundant
-    // 3. Search for the solution
-
-    this.board = new Board(mapData, itemsData);
+    // initialize the initial board, state, and starting player position
+    this.initBoard = new Board(mapData, itemsData);
     this.start_player_pos = searchValue(height, width, itemsData, BoardValues.PLAYER);
 
+    ArrayList<Coordinate> crate_pos_list = new ArrayList<>();
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        if (mapData[i][j] == BoardValues.CRATE.value)
+          crate_pos_list.add(new Coordinate(i, j));
+      }
+    }
+    this.initState = new State(crate_pos_list, initBoard, null);
+
+    // map out all the targets for future reference???????????????? JUST IN CASE?
     for (int i = 0; i < height; i++) {
       for (int j = 0; j < width; j++) {
         if (mapData[i][j] == BoardValues.TARGET.value)
@@ -147,12 +170,40 @@ public class SokoBot {
       }
     }
 
+    // Create the search tree in a DFS manner
+    Stack<State> stateStack = new Stack<>();
+    ArrayList<Push> pushList = new ArrayList<>();
 
-    return "lrlrlrlrlrlrlrlrlrlrlrlrlrlrlr";
+    if (isEnd(initState))
+      return "";
+
+    stateStack.push(initState);
+
+    while (!stateStack.empty()) {
+      State currState = stateStack.pop();
+      pushList.add(currState.prevPush);
+
+      if (isEnd(currState))
+        break;
+
+      for (State legalPushes : createLegalPush(currState)) {
+        stateStack.push(legalPushes);
+      }
+    }
+
+    if (pushList.size() == 0) {
+      System.out.println("No pushes");
+      return "";
+    }
+    for (Push push : pushList) {
+       System.out.println("Push Index: " + push.box_index + ", Direction: " + push.dir);
+    }
+    return "";
   }
 
   /**
    * FOR TESTING
+   *
    * @param args
    */
   public static void main(String[] args) {
--- src/solver/State.java
+++ src/solver/State.java
@@ -3,11 +3,14 @@ package solver;
 import java.util.ArrayList;
 
 public class State {
-    public ArrayList<Coordinate> box_pos_list;
-    public Board board;
 
-    public State(ArrayList<Coordinate> box_pos_list, Board board) {
-        this.box_pos_list = box_pos_list;
+    public ArrayList<Coordinate> crate_pos_list;
+    public Board board;
+    public Push prevPush;
+
+    public State(ArrayList<Coordinate> crate_pos_list, Board board, Push prevPush) {
+        this.crate_pos_list = crate_pos_list;
         this.board = board;
+        this.prevPush = prevPush;
     }
 }
